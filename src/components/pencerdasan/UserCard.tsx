3a6d8fb37fa9aa04dcd0930ece6aa735e624d0a6
mvinorian <vinorianmuhammad@gmail.com>
2023-01-20 04:43:36 +0700

    feat: add withSearch hoc

--- src/components/pencerdasan/UserCard.tsx
+++ src/components/pencerdasan/UserCard.tsx
@@ -2,7 +2,7 @@ import { User } from "@/types/pencerdasan/user";
 
 export default function UserCard(user: User) {
   return (
-    <div className="flex flex-col justify-start items-start border-2 border-black p-2 m-2 w-full">
+    <div className="flex flex-col justify-start items-start border-2 border-black p-2 my-2 w-full">
       <h1 className="text-2xl">
         <span className="font-bold">{user.username}</span> | {user.name}
       </h1>
--- src/pages/pencerdasan/users.page.tsx
+++ src/pages/pencerdasan/users.page.tsx
@@ -1,24 +1,13 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+import withSearch, {
+  WithSearchProps,
+} from "@/components/hoc/pencerdasan/withSearch";
 import UserCard from "@/components/pencerdasan/UserCard";
 import { User } from "@/types/pencerdasan/user";
 
-const queryClient = new QueryClient();
-
 export default function UsersPage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <UsersList />
-    </QueryClientProvider>
-  );
-}
-
-function UsersList() {
   const {
     isLoading,
     error,
@@ -31,15 +20,35 @@ function UsersList() {
   if (isLoading) return <>Loading...</>;
   if (error) return <>Error</>;
 
+  const filterUsers = (searchTerm: string) => {
+    searchTerm = searchTerm.toLowerCase();
+
+    return users?.data.filter((user) => {
+      const str = `${user.name} ${user.username} ${user.email}`.toLowerCase();
+      return str.indexOf(searchTerm) >= 0;
+    });
+  };
+
+  const UserListWithSearch = withSearch(UsersList, filterUsers);
+
   return (
     <div className="flex flex-col justify-start items-center">
-      <ul className="list-none flex flex-col justify-center items-stretch w-2/5">
-        {users?.data.map((user) => (
-          <li key={user.id}>
-            <UserCard {...user} />
-          </li>
-        ))}
-      </ul>
+      <div className="flex flex-col justify-start items-center w-7/12 p-2">
+        <UserListWithSearch />
+      </div>
     </div>
+  );
+}
+
+function UsersList(props: WithSearchProps) {
+  const { data } = props;
+  return (
+    <ul className="list-none flex flex-col justify-center items-stretch w-full">
+      {data?.map((user) => (
+        <li key={user.id}>
+          <UserCard {...user} />
+        </li>
+      ))}
+    </ul>
   );
 }
